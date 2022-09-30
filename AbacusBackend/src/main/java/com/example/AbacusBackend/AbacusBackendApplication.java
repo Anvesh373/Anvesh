26172edff6270db889d1bb41d32dfc0651bcdadf
DasariAnvesh <112835579+DasariAnvesh@users.noreply.github.com>
2022-09-30 21:09:36 +0530

    Backend code

---
+++ AbacusBackend/src/main/java/com/example/AbacusBackend/AbacusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.AbacusBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacusBackendApplication.class, args);
+	}
+
+}
---
+++ AbacusBackend/src/test/java/com/example/AbacusBackend/AbacusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AbacusBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbacusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
